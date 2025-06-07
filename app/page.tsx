5e80df7ec24d42577dc39dd0bb41b77ee84ad4dc
Jiaen Lee <nn750137@gmail.com>
2025-06-08 02:14:26 +0800

    docs: update post

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,15 @@ export default function Page() {
   return (
     <section>
       <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+        About me
+        <br />
+        李佳恩 Jia-en Lee (kaonlee)
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
+      <div className="space-y-4">
+        <p>嗨，我是佳恩，一位圖書資訊學出身的小小前端工程師。</p>
+        <p>喜歡美好的事物：自由、小熊貓、波加曼。</p>
+        <p>在這裡，我將記錄我的生活和學習。希望能為你帶來樂趣或幫助。</p>
+      </div>
       <div className="my-8">
         <BlogPosts />
       </div>
